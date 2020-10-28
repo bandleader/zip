13fe218e8f8b0f74b8afa7d528141bd333d8a77a
BL <1201678-bandleader@users.noreply.gitlab.com>
2020-10-28 16:09:58 +0200

    Remove RPC stuff from the about page

--- old/pages--About.vue
+++ old/pages--About.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
     <h3>About</h3>
-    <p>To get a greeting and test RPC, enter your name and click the button below.</p>
-    <p>
-      <input v-model="name" class="form-control">
-      <br>
-      <button type="button" class="btn btn-primary" @click="greet">RPC Greeting</button>
-    </p>
+    <p>This is the about page.</p>
   </div>
 </template>  
 
@@ -15,13 +10,6 @@ export default {
   pageTitle: "About",
   menuLink: true,
   data: () => ({
-    name: "world"
   }),
-  methods: {
-    async greet() { 
-      const rpcResult = await App.backend.greeting({name: this.name})
-      alert(rpcResult) 
-    }
-  }
 }
 </script>
