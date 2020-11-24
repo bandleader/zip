651b633be9f266f3b374ea9edd01dbb673a473e7
BL <1201678-bandleader@users.noreply.gitlab.com>
2020-11-24 02:13:58 +0200

    allow __ for route params

--- src/zip-runner.ts
+++ src/zip-runner.ts
@@ -167,7 +167,7 @@ export default class ZipRunner {
     scripts.push(`
       const routes = [
         { path: '/', component: window.vues['Home'] },
-        ${vuesPages.map(v =>`{ path: '/${v.componentKey}', component: window.vues['${v.componentKey}'] }`).join(", ")}
+        ${vuesPages.map(v =>`{ path: '/${v.componentKey.replace(/__/g,":")}', component: window.vues['${v.componentKey}'] }`).join(", ")}
       ]
       // Add special routes for components that declare one
       Object.values(window.vues).forEach(comp => {
