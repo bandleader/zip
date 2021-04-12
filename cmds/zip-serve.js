bebd2a6c153cb4d533baf5bd455f8e4faadca38f
BL <1201678-bandleader@users.noreply.gitlab.com>
2021-04-12 21:57:37 +0300

    Respect PORT environment variable

--- cmds/zip-serve.js
+++ cmds/zip-serve.js
@@ -3,7 +3,7 @@
 const fs = require('fs')
 const path = require('path')
 const http = require('http')
-const port = 3000
+const port = process.env.PORT || 3000 
 
 const cache = (factory, immed) => {
   let value = null, valid = false, first = true
