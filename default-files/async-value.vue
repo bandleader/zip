7cc24b8be6bc393ce6d197bfbfeb7cbcdc4a4cff
BL <1201678-bandleader@users.noreply.gitlab.com>
2021-07-08 02:43:46 +0300

    async value move out into component

---
+++ default-files/async-value.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-if="info.resolved"><slot v-bind="{value: info.value}" /></div>
+    <div v-else-if="info.error" class="text-danger"><i class="fa fa-exclamation-triangle" /> {{info.error}}</div>
+    <div v-else class="text-center"><div class="spinner-border text-primary" /></div>
+</template>
+
+<script>
+export default {
+  props: ['promise'],
+  computed: {
+    info() {
+      const ret = Vue.observable({
+        resolved: false,
+        error: null,
+        value: null
+      })
+      let promise = this.promise
+      if (typeof promise === 'function') promise = promise()
+      if (typeof promise !== 'object' || typeof promise.then !== 'function') throw "<async-value>: prop 'promise' must be a Promise/PromiseLike or a function returning one; got: " + typeof promise
+      promise.then(x => {ret.resolved = true; ret.value = x}, err => ret.error = err)
+      return ret
+    }
+  }  
+})
+</script>
