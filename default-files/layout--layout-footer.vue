3552d9b29e7200034e0ce3f153d3b8392c5054df
BL <1201678-bandleader@users.noreply.gitlab.com>
2021-07-07 19:45:38 +0300

    Move footer out

---
+++ default-files/layout--layout-footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer v-if="false" class="bg-primary text-light p-4" style="min-height: 10em">
+    <h3 style="text-transform: uppercase">&copy; {{$root.siteBrand}}</h3>
+  </footer>
+</template>
+
+<script>
+// By default no footer is shown (v-if="false") -- this file is only here as a starting point for you to modify if a footer is desired
+export default {
+}
+</script>
