38101e2b8fe0d0c57aaf824d0c39d82917d7287b
BL <1201678-bandleader@users.noreply.gitlab.com>
2021-07-07 23:45:35 +0300

    header: close nav on link click

--- default-files/layout/layout-header.vue
+++ default-files/layout/layout-header.vue
@@ -3,13 +3,13 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container-fluid">
         <router-link class="navbar-brand" to="/">{{$root.siteBrand}}</router-link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" @click="toggleNavBar">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation" @click="toggleNavBar()">
           <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse">
           <ul class="navbar-nav me-auto mr-auto"><!-- margins with both bs4 and bs5 syntax -->
-            <li class="nav-item" v-for="i in $root.navMenuItems">
+            <li class="nav-item" v-for="i in $root.navMenuItems" @click="toggleNavBar(false)">
               <router-link class="nav-link" :to="i.url" active-class="active">{{i.text}}</router-link>
             </li>
           </ul>
@@ -25,14 +25,18 @@
 <script>
 export default {
   methods: {
-    toggleNavBar() { toggleBootstrapNavbar(document.querySelector('.navbar-collapse')) }
+    toggleNavBar(action) { toggleBootstrapNavbar(document.querySelector('.navbar-collapse'), action) }
   }  
 }
 
 // Simple function to toggle a Bootstrap 4 navbar without Boostrap JS or even jQuery. Simplified from codepen.io/jasonbutz/pen/yPodOE
-function toggleBootstrapNavbar(targetEl) {
+function toggleBootstrapNavbar(targetEl, action = 'toggle') {
+  // action can be true, false, or 'toggle' (default)
   var cl = targetEl.classList
   var isShown = cl.contains("show") || cl.contains("collapsing")
+  var shouldShow = action === 'toggle' ? !isShown : !!action
+  if (isShown === shouldShow) return;
+
   if (isShown) { 
     targetEl.style.height = targetEl.getBoundingClientRect().height + "px"
     targetEl.offsetHeight // force reflow
