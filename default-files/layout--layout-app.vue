c153d68fb2f0c3176dfd7e07446565717410fe9e
BL <1201678-bandleader@users.noreply.gitlab.com>
2021-07-07 19:32:06 +0300

    layout: move header out

--- default-files/layout--layout-app.vue
+++ default-files/layout--layout-app.vue
@@ -1,24 +1,6 @@
 <template>
   <div>
-    <header>
-      <div class="bs-component">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-          <router-link class="navbar-brand" to="/">{{$root.siteBrand}}</router-link>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" @click="toggleNavBar">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item" v-for="i in $root.navMenuItems">
-                <router-link class="nav-link" :to="i.url">{{i.text}}</router-link>
-              </li>
-            </ul>
-            <login-section />
-          </div>
-        </nav>
-      </div>
-    </header>
+    <layout-header v-if="!noheader" />
     <main class="container" style="margin: 2em 0">
       <slot />
     </main>
@@ -49,6 +31,9 @@ function toggleBootstrapNavbar(targetEl) {
 }
 
 export default {
+  props: { 
+    noheader: { type: Boolean, default: false }
+  },
   methods: {
     toggleNavBar() { toggleBootstrapNavbar(document.querySelector('.navbar-collapse')) }
   }
---
+++ default-files/layout--layout-header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="bs-component">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <router-link class="navbar-brand" to="/">{{$root.siteBrand}}</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" @click="toggleNavBar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item" v-for="i in $root.navMenuItems">
+              <router-link class="nav-link" :to="i.url">{{i.text}}</router-link>
+            </li>
+          </ul>
+          <login-section />
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
